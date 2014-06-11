53fcc67c807322541edcd4508cb42e7a316af905
Françoise Leurent <fleurent@ivsweb.com>
2014-06-11 09:45:47 +0200

    Expose error callback

--- src/node/child_process.js
+++ src/node/child_process.js
@@ -1,7 +1,8 @@
 var cp = require('child_process');
 
   //child_process.exec only wait for console, this use process spawn
-cp.exec_window = function(cmd, args, callback){
+cp.exec_window = function(cmd, args, callback, error){
   var ps = cp.spawn(cmd, args);
   ps.on('close', callback);
+  ps.on('error', error || function(){} );
 }
