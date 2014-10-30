48e479a8220fec66e1ae2aa9897c29a3e3e1de93
Francois Leurent <fleurent@ivsweb.com>
2014-10-30 06:34:42 +0100

    Expose Function.static binding

--- src/natives.js
+++ src/natives.js
@@ -1,3 +1,18 @@
+
+Function.prototype.static = function(){
+  this.$static = true;
+  return this;
+}
+
+var legacy = Class.prototype.implement;
+Class.implement('implement', function(k,v){
+  if(v && v.$static) this[k] = v;
+  else legacy.apply(this, [k, v]);
+}.overloadSetter());
+
+
+
+
 Buffer.implement({
   indexOf:function(chr) {
     for (var i = 0; i < this.length ; i++)
