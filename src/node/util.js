e9b0a497e61388b7e00df34f5421963dd5202cf4
François Leurent <131.php@cloudyks.org>
2015-05-19 23:38:03 +0200

    Default value for util.parseargs

--- src/node/util.js
+++ src/node/util.js
@@ -7,7 +7,10 @@ util.md5 = function(str){
 }
 
 
-util.parseargs = function(argv){
+process.parseargs = util.parseargs = function(argv){
+  if(arguments.length == 0)
+     argv = process.argv.slice(2);
+
   var args = [], dict = {},
       k, v, r, e = new RegExp("^--?([a-z_0-9/:-]+)(?:=(.*))?", "i");
 
