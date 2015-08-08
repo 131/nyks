1b3d15c2bc9c3291e11f469a56c643ad5b9f6a52
Francois Leurent <131.js@cloudyks.org>
2015-08-08 22:59:12 +0200

    DOS2UNIX - Use crypto to generate randomness is tmppath - tmppath cleanup paths if exists - fixes for pemme (allow buffers)

--- fs/tmppath.js
+++ fs/tmppath.js
@@ -1,14 +1,48 @@
 var fs   = require('fs');
 var path = require('path');
 var os   = require('os');
+var crypto = require('crypto');
 
-var randString   = require('mout/random/randString');
+var exitListenerAttached = false;
+var filesToDelete = [];
 
-var tmppath = module.exports = function(ext){
-  ext = ext || "tmp";
-  var fname = ext + "-" + randString(8) + "." + ext;
+function deleteOnExit(file_path) {
+
+  if (!exitListenerAttached) {
+    console.log("Registering for cleanup");
+    process.on('exit', cleanupFilesSync);
+      //makes sure exit is called event on sigint \o/
+    process.on('SIGINT', process.exit);
+    exitListenerAttached = true;
+  }
+
+  filesToDelete.push(file_path);
+}
+
+
+function cleanupFilesSync() {
+  while ((toDelete = filesToDelete.shift()) !== undefined) {
+    if(fs.existsSync(toDelete))
+      fs.unlinkSync(toDelete);
+  }
+}
+
+  
+
+
+var tmppath = module.exports = function(ext, len, trash){
+  len = len || 8; ext = ext || "tmp";
+  if(trash === undefined) trash = true;
+
+
+  var body = crypto.randomBytes(len).toString('base64').replace(/\//g, '+').substr(0, len);
+  var fname = ext + "-" + body + "." + ext;
   var file_path = path.join(os.tmpdir(), fname);
 
-  return fs.existsSync(file_path) ? tmppath(ext) : file_path;
+  var fullpath = fs.existsSync(file_path) ? tmppath(ext) : file_path;
+  if(trash)
+    deleteOnExit(fullpath);
+
+  return fullpath;
 }
 
