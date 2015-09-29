a0c255dc3475a5487a862409436167543c5170d9
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-09-29 18:00:16 +0200

    add hexToRgb

--- string.js
+++ string.js
@@ -1,13 +1,14 @@
 'use strict';
 
 module.exports = {
-    'chunk'    : require('./string/chunk'),
-    'replaces' : require('./string/replaces'),
-    'rot13'    : require('./string/rot13'),
-    'rreplaces': require('./string/rreplaces'),
-    'sprintf'  : require('./string/sprintf'),
-    'stripEnd' : require('./string/stripEnd'),
-    'crc32'    : require('./string/crc32'),
-    'stripStart' : require('./string/stripStart')
+    'chunk'      : require('./string/chunk'),
+    'replaces'   : require('./string/replaces'),
+    'rot13'      : require('./string/rot13'),
+    'rreplaces'  : require('./string/rreplaces'),
+    'sprintf'    : require('./string/sprintf'),
+    'stripEnd'   : require('./string/stripEnd'),
+    'crc32'      : require('./string/crc32'),
+    'stripStart' : require('./string/stripStart'),
+    'hexToRgb'   : require('./string/hexToRgb')
 };
 
