76e00ca6ad8f77e89900499dc04d73e51470ae3c
François Leurent <131.php@cloudyks.org>
2016-06-06 21:39:12 +0200

    Bufferutil helpers

--- test/buffer.js
+++ test/buffer.js
@@ -2,6 +2,36 @@
 
 var expect = require('expect.js')
 var indexOf = require('../buffer/indexOf')
+var writeBits = require('../buffer/writeBits');
+var fromInt   = require('../buffer/fromInt');
+
+
+function trace(buf){
+  var foo = require('mout/array/map')(buf).map((v) => { return ("00000000" + v.toString(2)).substr(-8) });
+  console.log(foo);
+}
+
+
+describe("Buffer writeBits", function(){
+  it("should test writeBits", function(){
+     // TOUS LES OFFSETS SONT INCLUSIFS (taille mini 1) (offsetEnd = offsetStart + length - 1)
+    expect(writeBits(Buffer([1]), Buffer(0), 1)).to.eql(Buffer([1]));// 01100000
+    expect(writeBits(Buffer([0]), Buffer([255]), 1)).to.eql(Buffer([127]));// 01100000
+
+    expect(writeBits(Buffer([0]), Buffer([255]), 1, 2)).to.eql(Buffer([96]));// 01100000
+    expect(writeBits(Buffer([0, 195]), fromInt(3), 11, -2)).to.eql(Buffer([0, 219]));
+    expect(writeBits(Buffer([0,0,0, 195]), fromInt(3), 27, -2)).to.eql(Buffer([0,0,0, 219]));
+    expect(writeBits(Buffer([0]), Buffer([192]), 0, 2)).to.eql(Buffer([192]));
+
+    // [195] 11000011
+    // [219] 11011011 (write 3 at 4)
+
+
+    // [129, 5, 39] 10000001 00000101 00100111
+    // [129, 5, 39] 10001000 10000101 00100111  (write 17 at 4)
+    expect(writeBits(Buffer([129,5,39]), fromInt(17), 4, -5)).to.eql(Buffer([136,133,39]));
+  });
+});
 
 
 describe("Crypto Buffer indexOf", function(){
