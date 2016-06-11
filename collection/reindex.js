7f30fdae0cd15956dc315f735d19f7dc7aa09798
François Leurent <131.php@cloudyks.org>
2016-06-11 11:19:10 +0200

    Fix reindex (using dicts)

--- collection/reindex.js
+++ collection/reindex.js
@@ -2,13 +2,14 @@
 
 var forIn = require('mout/object/forIn');
 
-module.exports = function(that, key){
+module.exports = function(that, key, column){
+
   let out = {};
   let iteratee = Array.isArray(that)
-        ? Array.prototype.forEach(that)
+        ? Array.prototype.forEach.bind(that)
         : forIn.bind(null, that);
   iteratee(function(v, k){
-    out[v[key]] = v;
+    out[v[key]] = column == undefined ? v : v[column];
   });
 
   return out;
