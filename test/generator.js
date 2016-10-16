7fb64b80479519dcf8bfa3cb1dfc40039e805665
François Leurent <fleurent@ivsweb.com>
2016-10-16 11:23:45 +0200

    Test generator w/ curry

--- test/generator.js
+++ test/generator.js
@@ -8,24 +8,28 @@ const eachLimit = require('async-co/eachLimit');
 
 describe("testing generator", function() {
 
-  it("should test generator func", function *(){
-
-
     class Foo {
       constructor(start) {
         this.tick = start;
       }
-      * walk () {
+      * walk (step) {
         yield new Promise(setImmediate);
-        this.tick ++;
+        this.tick += step || 1;
         return this.tick;
       }
     }
 
-
+  it("should test generator func", function *(){
     var foos = range(1,10).map( i => new Foo(i) );
     var ticks = yield eachLimit(foos, 2, func('walk'));
     expect(ticks).to.eql([2,3,4,5,6,7,8,9,10,11]);
+  });
+
+
+  it("should handle test generator func curry", function *(){
+    var foos = range(1,10).map( i => new Foo(i) );
+    var ticks = yield eachLimit(foos, 2, func('walk', 2));
+    expect(ticks).to.eql([3,4,5,6,7,8,9,10,11, 12]);
   });
 
 
