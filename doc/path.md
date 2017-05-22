b248fc5ab7b5819f231091bedb427ee41a275596
François Leurent <fleurent@ivsweb.com>
2017-05-22 15:12:18 +0200

    With path/url

--- doc/path.md
+++ doc/path.md
@@ -4,3 +4,8 @@ Search for a binary in env PATH
 
 * require('nyks/path/extend')( path[,path2, ..]);
 Extend system PATH with new directories
+
+
+* require('nyks/path/url')( file_path);
+Return a path with a file:// scheme syntax
+
---
+++ path/url.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const path = require('path');
+
+module.exports = function fileUrl(str) {
+
+  var pathName = path.resolve(str).replace(/\\/g, '/');
+
+  // Windows drive letter must be prefixed with a slash
+  if (pathName[0] !== '/')
+      pathName = '/' + pathName;
+
+  return encodeURI('file://' + pathName);
+};
