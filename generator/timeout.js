0ff486553a16b0a01f63aead816531609fcb6b17
François Leurent <fleurent@ivsweb.com>
2017-10-02 18:04:18 +0200

    +generator/timeout

---
+++ generator/timeout.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const defer      = require('../promise/defer');
+const isNumber   = require('mout/lang/isNumber');
+
+module.exports =  function(fn, timeout, ctx) {
+  if(!timeout)
+    return fn;
+
+  if(!isNumber(timeout))
+    throw "timeout must be a number";
+
+  var my = function* () {
+    var args = [].slice.call(arguments);
+    var self = ctx || this;
+    var defered = defer();
+
+    setTimeout(defered.reject.bind(defered, "timeout"), timeout);
+
+    yield [ function*() {
+      var response = yield fn.apply(self, args);
+      defered.resolve(response);
+    }, defered];
+
+    return defered;
+  }
+  return my;
+}
