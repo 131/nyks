f80ca430108af79d0d5482b0c59fc8c81c573021
Mejdi Ourir <m.ourir@ivsweb.com>
2017-10-03 11:03:44 +0200

    typo name function

--- fs/writeLazySafeSync.js
+++ fs/writeLazySafeSync.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs         = require('fs');
-const mkdirpsync = require('./mkdirpsync')
+const mkdirpsync = require('./mkdirpSync');
 const path       = require('path');
 
 module.exports = (file_path, body) => {
