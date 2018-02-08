d7c3ab2f1e1cbbdd22c051b3c970d7e1347c3eb9
Florent Kolosowski <fkolosowski@ivsweb.com>
2018-02-08 10:44:42 +0100

    Add tests for Array/PickIn & Array/reindex

--- test/array.js
+++ test/array.js
@@ -4,18 +4,42 @@
 
 const expect = require('expect.js');
 
-//const pickIn  = require('../array/pickIn');
-//const reindex = require('../array/reindex');
+const pickIn  = require('../array/pickIn');
+const reindex = require('../array/reindex');
 const sum     = require('../array/sum');
 
 describe("Array functions", function() {
 
   it("should test sum", function() {
-    expect(sum([1,2,3,4])).to.be(10);
+    expect(sum([1, 2, 3, 4])).to.be(10);
   });
 
   it("should test sum of empty array", function() {
     expect(sum([])).to.be(0);
+  });
+
+  it("should test pickIn", function() {
+    expect(pickIn('b', ['a', 'b', 'c', 'd'])).to.be('b');
+  });
+
+  it("should test pickIn with wrong value", function() {
+    expect(pickIn('melon', ['a', 'b', 'c', 'd'])).to.be('a');
+  });
+
+  it("should test reindex", function() {
+    var people = [
+      {name : 'Jean Lebon',   age : 12, town : 'Paris'},
+      {name : 'John TheGood', age : 22, town : 'Dallas'},
+      {name : 'Juan ElBueno', age : 35, town : 'Madrid'},
+    ];
+
+    var target = {
+      'Jean Lebon'   : {name : 'Jean Lebon',   age : 12, town : 'Paris'},
+      'John TheGood' : {name : 'John TheGood', age : 22, town : 'Dallas'},
+      'Juan ElBueno' : {name : 'Juan ElBueno', age : 35, town : 'Madrid'}
+    };
+
+    expect(reindex(people, 'name')).to.eql(target);
   });
 
 });
