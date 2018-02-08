e7ff2d2aa3389e110a4d772e88261f05ad0040dc
Florent Kolosowski <fkolosowski@ivsweb.com>
2018-02-08 12:52:27 +0100

    Add tests for Crypto/pemme

--- test/crypto.js
+++ test/crypto.js
@@ -6,7 +6,7 @@ const expect = require('expect.js');
 
 const md5         = require('../crypto/md5');
 //const openssh2pem = require('../crypto/openssh2pem');
-//const pemme       = require('../crypto/pemme');
+const pemme       = require('../crypto/pemme');
 const sha1        = require('../crypto/sha1');
 
 describe("Crypto testing functions", function() {
@@ -16,5 +16,13 @@ describe("Crypto testing functions", function() {
 
   it("should test sha1", function() {
     expect(sha1("Hello")).to.be("f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0");
+  });
+
+  it("should test pemme", function() {
+    expect(pemme("Hello", "WRAPPER")).to.be(`-----BEGIN WRAPPER-----\nHello\n-----END WRAPPER-----`);
+  });
+
+  it("should test pemme from Buffer", function() {
+    expect(pemme(new Buffer("Hello"), "WRAPPER")).to.be(`-----BEGIN WRAPPER-----\nSGVsbG8=\n-----END WRAPPER-----`);
   });
 });
