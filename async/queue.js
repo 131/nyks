93ede62878b683929bcfaa22f293afe012866834
Florent Kolosowski <fkolosowski@ivsweb.com>
2018-02-20 10:38:39 +0100

    expose workerChain in queue

--- async/queue.js
+++ async/queue.js
@@ -3,30 +3,31 @@
 //st exupery style
 
 module.exports = function(thunk, workers) {
-  var workerChain = [];
 
-  var process = async function(task) {
+  var process = async function() {
     await pickworker();
-    var ret = await thunk.call(this, task);
+    var ret = await thunk.apply(this, arguments);
     freeworker();
     return Promise.resolve(ret);
   };
 
+  var out         = process;  // better candidate than {}
+  out.push        = process; // per compatibility
+  out.workerChain = [];
+
   var pickworker = function () {
     if(!workers) //no available worker, waiting
       return new Promise(function(resolve) {
-        workerChain.push(resolve);
+        out.workerChain.push(resolve);
       });
     return Promise.resolve(workers--); //worker id
   };
 
   var freeworker = function() {
     workers++;
-    if(workerChain.length)
-      workerChain.shift()(pickworker());
+    if(out.workerChain.length)
+      out.workerChain.shift()(pickworker());
   };
 
-  var out  = process;  // better candidate than {}
-  out.push = process; // per compatibility
   return out;
 };
