c7767064c8bbb9d87d1c82fb007f7b6f8ab88a54
François Leurent <131.js@cloudyks.org>
2019-08-29 23:20:09 +0200

    With divev2

--- object/dive.js
+++ object/dive.js
@@ -8,6 +8,13 @@ function dive(obj) { //..., path
   var parts = [].slice.call(arguments, 1);
   for(let part of parts) {
     if(part == null) return;
+    if(obj == null) return;
+
+    if(obj[part] != null) {
+      obj = obj[part];
+      continue;
+    }
+
     for(let spart of String(part).split('.')) {
       if(obj == null) return;
       obj = obj[spart];
