234e383e0d39d51a9ee2fa7cf595fb27dfd4bbda
Francois Leurent <131.code@leurent.email>
2023-05-12 11:41:00 +0200

    FW options

--- stream/pipe.js
+++ stream/pipe.js
@@ -2,22 +2,22 @@
 
 //pipe a stream to another, streams might be promises
 
-function pipe(src, dest) {
+function pipe(src, dest, options) {
 
   if(src.then) {
     return src.then(function(tmp) {
-      return pipe(tmp, dest);
+      return pipe(tmp, dest, options);
     });
   }
 
   if(dest.then) {
     return dest.then(function(tmp) {
-      return pipe(src, tmp);
+      return pipe(src, tmp, options);
     });
   }
 
   return new Promise(function(resolve, reject) {
-    src.pipe(dest);
+    src.pipe(dest, options);
     src.on('error', reject);
     if(!("fd" in dest))
       dest.on('finish', resolve);
